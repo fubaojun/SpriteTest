389e707683330caa23b9dc8ba75dce0cef54e726
fubaojun2006 <fubaojun2006@139.com>
2017-10-25 12:09:04 +0800

    Initial commit -- can run NOW!

---
+++ app/src/main/java/com/iothouse/spritetest/GameSurfaceView.java
@@ -0,0 +1,142 @@
+package com.iothouse.spritetest;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.SurfaceHolder;
+
+import java.io.InputStream;
+
+/**
+ * Created by bjfu on 2017/10/24.
+ */
+
+public class GameSurfaceView extends SurfaceView implements SurfaceHolder.Callback {
+    private String TAG = "GameSurfaceView";
+
+    public Bitmap mBitmapResource = null;
+    public Bitmap[][] mBitmaps = null;
+    public Paint mPaint = null;
+    public SurfaceHolder mSurfaceHolder = null;
+
+    private DrawThread mDrawThread = null;
+
+    public int spriteWidth = 0;
+    public int spriteHeight = 0;
+
+    public GameSurfaceView(Context context) {
+        super(context);
+        Log.e(TAG,"-- GameSurfaceView --");
+        mSurfaceHolder = this.getHolder();
+        mSurfaceHolder.addCallback(this);//必须做这一步，不然 那个回调函数不会调用也不出图
+
+        BitmapFactory.Options opt = new BitmapFactory.Options();
+        opt.inPreferredConfig = Bitmap.Config.RGB_565;
+        opt.inPurgeable = true;
+        opt.inInputShareable = true;
+
+        @SuppressLint("ResourceType") InputStream is = context.getResources().openRawResource(R.drawable.sprite);
+        mBitmapResource = BitmapFactory.decodeStream(is, null, opt);
+        mBitmaps = generateBitmapArray(mBitmapResource, 4, 4);
+        mPaint = new Paint();
+        mPaint.setColor(Color.GREEN);// 画笔为绿色
+        mPaint.setTextSize(40);
+        mPaint.setStrokeWidth(2);// 设置画笔粗细
+    }
+
+    public Bitmap[][] generateBitmapArray(Bitmap bitmapSRC, int row, int col){
+        Bitmap[][] bitmaps = new Bitmap[row][col];
+        this.spriteWidth = bitmapSRC.getWidth()/row;
+        this.spriteHeight = bitmapSRC.getHeight()/row;
+
+        for (int i=0; i<row; i++){
+            for (int j=0; j<col; j++){
+                bitmaps[i][j] = Bitmap.createBitmap(bitmapSRC,
+                        this.spriteWidth*j, this.spriteHeight*i,
+                        this.spriteWidth, this.spriteHeight);
+
+            }
+        }
+
+        return bitmaps;
+    }
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        Log.e(TAG,"-- surfaceCreated --");
+        Canvas canvas = holder.lockCanvas();
+        canvas.drawBitmap(mBitmapResource,0,0,null);
+        Log.e(TAG,"-- surfaceCreated -drawBitmap-");
+        holder.setFixedSize(500,660);//这里会把Surface 缩放到 SurfaceView 上显示
+        Log.e(TAG,"-- surfaceCreated -setFixedSize-");
+        float x = 500;
+        float y = 660;
+
+        canvas.drawLine(0,0,x,y,mPaint);
+        canvas.drawLine(0,y,x,y,mPaint);
+        canvas.drawLine(x,0,x,y,mPaint);
+        canvas.drawText(""+x+","+y,120,120,mPaint);
+        holder.unlockCanvasAndPost(canvas);
+
+        if(null == mDrawThread){
+            mDrawThread = new DrawThread();
+            mDrawThread.start();
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        Log.e(TAG,"-- surfaceChanged --");
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        Log.e(TAG,"-- surfaceDestroyed --");
+
+    }
+
+    private class DrawThread extends Thread {
+        public boolean isRunning = false;
+        public float location_x = 0;
+        public float location_y = 0;
+        public int location_step = 0;
+        public static final int total_step = 4;
+
+        public DrawThread() {
+            this.isRunning = true;
+        }
+
+        @Override
+        public void run() {
+            Canvas canvas = null;
+            while (isRunning){
+                if (++location_step == total_step) {
+                    location_step = 0;
+                }
+                location_x = -(330*location_step/total_step);
+//                location_y = -(250*location_step/total_step);
+
+                canvas = mSurfaceHolder.lockCanvas();
+                canvas.drawColor(Color.BLACK);
+//                canvas.drawBitmap(mBitmapResource,location_x,location_y,null);
+                canvas.drawBitmap(mBitmaps[0][location_step], 0,0,null);
+                canvas.drawBitmap(mBitmaps[1][location_step], 0,300,null);
+                canvas.drawBitmap(mBitmaps[2][location_step], 300,0,null);
+                canvas.drawBitmap(mBitmaps[3][location_step], 300,300,null);
+                mSurfaceHolder.unlockCanvasAndPost(canvas);
+
+                try {
+                    Thread.sleep(150);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/iothouse/spritetest/MainActivity.java
@@ -0,0 +1,60 @@
+package com.iothouse.spritetest;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        float x = getWindowManager().getDefaultDisplay().getWidth();
+        //虚拟按键的显示与隐藏会影响这个值 显示时：1776  隐藏时：1920
+        float y = getWindowManager().getDefaultDisplay().getHeight();
+
+        GameSurfaceView gameView = new GameSurfaceView(this);
+        setContentView(gameView);
+
+//        setContentView(R.layout.activity_main);
+//        addContentView();
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
